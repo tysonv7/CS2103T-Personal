eecd6bf044d524d8b18d563228aa99f7f0e13a1e
Tysonv7 <tyson_v7@hotmail.com>
2015-08-26 09:01:14 +0800

    Added whitespace at line 256

--- CS2103T Tutorial 2 - JavaCityConnectForRefactoring/src/CityConnect.java
+++ CS2103T Tutorial 2 - JavaCityConnectForRefactoring/src/CityConnect.java
@@ -253,7 +253,7 @@ public class CityConnect {
 		String newEndLocation = parameters[PARAM_POSITION_END_LOCATION];
 		String distance = parameters[PARAM_POSITION_DISTANCE];
 
-		if (!isPositiveNonZeroInt(distance)){
+		if (!isPositiveNonZeroInt(distance)) {
 			return String.format(MESSAGE_INVALID_FORMAT, userCommand);
 		}
 
