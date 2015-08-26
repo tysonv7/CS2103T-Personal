9afa798ad0128ed25bde757690ce7d179a1e8eb8
Tysonv7 <tyson_v7@hotmail.com>
2015-08-26 15:35:49 +0800

    Further refactoring in class

--- CS2103T Tutorial 2 - JavaCityConnectForRefactoring/src/CityConnect.java
+++ CS2103T Tutorial 2 - JavaCityConnectForRefactoring/src/CityConnect.java
@@ -48,7 +48,7 @@ public class CityConnect {
 	private static final String MESSAGE_NO_ROUTE = "No route exists from %1$s to %2$s!";
 	private static final String MESSAGE_ADDED = "Route from %1$s to %2$s with distance %3$skm added";
 	private static final String MESSAGE_INVALID_FORMAT = "invalid command format :%1$s";
-	private static final String WELCOME_MESSAGE = "Welcome to SimpleRouteStore!";
+	private static final String MESSAGE_WELCOME = "Welcome to SimpleRouteStore!";
 	private static final String MESSAGE_NO_SPACE = "No more space to store locations";
 
 	// These are the possible command types
@@ -98,11 +98,14 @@ public class CityConnect {
 	 * ====================================================================
 	 */
 	public static void main(String[] args) {
-		showToUser(WELCOME_MESSAGE);
+		showToUser(MESSAGE_WELCOME);
+		runUiLoop();
+	}
+
+	private static void runUiLoop() {
 		while (true) {
 			System.out.print("Enter command:");
-			String command = scanner.nextLine();
-			String userCommand = command;
+			String userCommand = scanner.nextLine();
 			String feedback = executeCommand(userCommand);
 			showToUser(feedback);
 		}
